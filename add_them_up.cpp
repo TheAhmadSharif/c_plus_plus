8253617a61c93feced4d5c24d9943abd6661b975
Ahmad Sharif <ahmad.sharif@tuni.fi>
2022-04-17 21:40:04 +0300

    Toph

---
+++ add_them_up.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	int a;
+	int b;
+	int sum;
+	std::cin >> a;
+	std::cin >> b;
+	sum = a + b;
+	std::cout << sum;
+	return 0;
+}
---
+++ copycat.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+	https://community.toph.co/t/copycat/1322
+*/
+
+int main() {
+	int x;
+	std::cin >> x;
+	std::cout << x;
+	return 0;
+}
